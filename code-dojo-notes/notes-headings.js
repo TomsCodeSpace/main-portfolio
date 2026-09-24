6b60247bf526bba2560aca046b6c428594d44226
Tom's Code Space <thomasmusic7575@gmail.com>
2026-09-24 11:50:30 -0500

    Add files via upload
    Added new files

---
+++ code-dojo-notes/notes-headings.js
@@ -0,0 +1,49 @@
+﻿// This controls collapsible headings
+
+
+// MAIN HEADINGS
+const headings = document.querySelectorAll(".heading");
+
+headings.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const content = button.nextElementSibling;
+
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        }
+        else {
+            content.style.display = "block";
+        }
+
+    });
+
+});
+
+// SUBHEADINGS
+const subheadings = document.querySelectorAll(".subheading");
+
+subheadings.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const subcontent = button.nextElementSibling;
+
+        if (subcontent.style.display === "block") {
+            subcontent.style.display = "none";
+        }
+        else {
+            subcontent.style.display = "block";
+        }
+
+    });
+
+});
+
+document.querySelectorAll(".heading").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const content = btn.nextElementSibling;
+        content.classList.toggle("open");
+    });
+});
